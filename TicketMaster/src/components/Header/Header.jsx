21a44af4b15691d350d8d64f89fc2b61f1fdbc0b
riessz <arthurriess10@gmail.com>
2025-06-14 17:55:33 -0300

    header update

--- TicketMaster/src/components/Header/Header.jsx
+++ TicketMaster/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="logo">🎟️ TicketSeller</div>
       <nav className="nav-links">
         <a href="/organizadores">Organizadores</a>
-        <a href="/meus-ingressos">Ingressos</a>
+        <a href="/meusingressos">Ingressos</a>
         <a href="/contato">Contato</a>
         <span className="cart-icon">🛒</span>
       </nav>
